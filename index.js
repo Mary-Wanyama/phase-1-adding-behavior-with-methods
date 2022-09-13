7377fa83e951aef9f39457bf6655a862bc1989c0
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-13 05:51:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+class cat{
+    constructor(name, sex){
+        this.name : name
+        this.sex : sex
+
+    }
+    speak(){return `${this.name} says Meow`}
+}
+class dog{
+    constructor(name, sex){
+        this.name : name
+        this.sex : sex
+    }
+    speak(){return `${this.name} says woof`}
+}
+class bird{
+    constructor(name, sex){
+        this.name : name
+        this.sex : sex
+    }
+    speak(){
+        if (this.sex ===male) {
+            return `it is me ${this.name} the parrot!`
+        }
+        else if (this.sex ===female){
+            return `${this.name} says squawks`
+        }
+        else{
+            return `${this.name} says squawk`
+        }
+    }
+}
